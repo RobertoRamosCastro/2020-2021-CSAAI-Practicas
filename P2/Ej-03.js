4bd70a81483ae4ab287a8c3db4e5e2b81f9f9bb8
Roberto <r.ramos.2017>
2021-03-11 10:12:02 +0100

    Pruebas

--- P2/Ej-03.js
+++ P2/Ej-03.js
@@ -14,6 +14,10 @@ console.log("Ejecutando js...")
 
 //-- Leer el párrafo identificado como test
 const test = document.getElementById('test')
+//-- Leer el elemento párrafo test2
+const test2 = document.getElementById('test2')
+//-- Obtener el elemento párrafo 1 para modificarlo
+const test1 = document.getElementById('test1')
 
 //-- Configurar el manejador para el evento de
 //-- pulsación de botón
@@ -21,4 +25,13 @@ const test = document.getElementById('test')
 // pero asi nos ahorramos escribir function
 test.onclick = () => {
   console.log("Click sobre el párrafo...")
+}
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón
+test2.onclick = () => {
+  console.log("Click sobre el párrafo 2...")
+
+  //-- Cambiar su texto
+  test1.innerHTML = "¡TEXTO CAMBIADO!"
 }
