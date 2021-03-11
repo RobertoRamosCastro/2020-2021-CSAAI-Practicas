af5a0056aa6bc7d251bae6e44ef098965a2520a2
Roberto <r.ramos.2017>
2021-03-11 09:57:55 +0100

    Pruebas

--- P2/Ej-03.js
+++ P2/Ej-03.js
@@ -7,3 +7,22 @@ console.log("Se recomienda hacerlo así");
 console.log("------");
 const test = document.getElementById("Id")
 console.log(test.innerHTML)
+
+window.alert("Hola ventana emergente")
+
+//-- Manejador del evento click sobre el párrafo test
+//-- Cada vez que se hace click en el se invoca a esta funcion
+function manejador_parrafo()
+{
+  console.log("Click sobre el párrafo...")
+}
+
+console.log("Ejecutando js...")
+
+//-- Leer el párrafo identificado como test
+const test = document.getElementById('test')
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón: que se ejecute la
+//-- funcion manejador_parrafo()
+test.onclick = manejador_parrafo;
