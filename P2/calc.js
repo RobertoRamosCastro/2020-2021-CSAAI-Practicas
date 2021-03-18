d3f28deba23c353f9f418c040a2872d1f220472f
Roberto <r.ramos.2017>
2021-03-18 09:32:00 +0100

    Anotaciones

--- P2/calc.js
+++ P2/calc.js
@@ -1,21 +1,23 @@
-//-- Contador de clicks de boton
-
 console.log("Ejecutando JS...");
 
-//-- Acceder a los elementos del DOM
-const display = document.getElementById("display");
-const boton = document.getElementById("boton");
+//-- Crear objeto gui, con los elementos de la interfaz gráfica
+//-- Al tenerlo agrupado podemos pasarlo como parámetro o asignárselo
+//-- a otro objeto
+const gui = {
+  display: document.getElementById("display"),
+  boton: document.getElementById("boton"),
+}
 
-//-- Contador de clicks
-let cont = 0;
+//-- Objeto contador: Contiene el valor y el método para incrementarse
+const counter = {
+  valor: 0,
+  inc : function(value) { 
+    this.valor += value; // se incrementa el value que le demos
+    gui.display.innerHTML = this.valor; // me lo actualizas en el display
+  } // con esto hemos definido un obeto con metodo, accion y valor del contador
+}
 
-//-- Configurar retrollamada del boton
-boton.onclick = () => {
-  console.log("Click!");
-
-  //-- Incrementar contador
-  cont += 1;
-
-  //-- Actualizar el display
-  display.innerHTML += ' ' + cont;
+//-- Acciones: Ligar el boton al contador
+gui.boton.onclick = () => { 
+  counter.inc(1) // llamamos al metodo inc del objeto contador
 }
