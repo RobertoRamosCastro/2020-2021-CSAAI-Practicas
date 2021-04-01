e66b0ade2022b646239fb1056b0d954d04af8775
Roberto <r.ramos.2017>
2021-04-01 19:18:58 +0200

    casi terminado

--- P2/calc1.js
+++ P2/calc1.js
@@ -1,35 +1,17 @@
 // botones de la calcu
 division = document.getElementById("division")
-igual = document.getElementById("igual")
+igual = document.getElementById("equal")
 suma = document.getElementById("suma")
 resta = document.getElementById("resta")
 multiplicacion = document.getElementById("multiplicacion")
-exponencial = document.getElementById("botonEXP")
-del =  document.getElementById("botonDELl")
-ac = document.getElementById("botonAC")
-dot = document.getElementById("dot")
+del =  document.getElementById("borrar")
+ac = document.getElementById("ac")
 result = document.getElementById("result")
 
 // array con los tipos de digitos
 let boton_value = document.getElementsByClassName("boton_value");
 // array con los tipos de operaciones
 let op = document.getElementsByClassName("op");
-
-function operador(boton)
-{
-  if (display.innerHTML=="0") {
-    display.innerHTML = boton.op;
-  }else {
-    display.innerHTML += boton.op;
-  }
-}
-
-// bucle para recoger botones con calss op
-for(i=0; i<op.length; i++){
-  op[i].onclick=(ev)=>{
-    operador(ev.target);
-  }
-}
 
 function digito(boton)
 {
@@ -40,6 +22,13 @@ function digito(boton)
   }
 }
 
+// bucle para recoger botones con calss op
+for(i=0; i<op.length; i++){
+    op[i].onclick=(ev)=>{
+      digito(ev.target);
+    }
+  }
+
 // bucle para recoger los botones con class boton_value
 for(i = 0; i < boton_value.length; i++){
     boton_value[i].onclick = (ev) =>{
@@ -47,47 +36,17 @@ for(i = 0; i < boton_value.length; i++){
     }
   }
 
-// Insertar +
-suma.onclick = () => {
-  display.innerHTML += "+";
-}
-
-// Insertar -
-resta.onclick = () => {
-  display.innerHTML += "-";
-}
-
-// Insertar *
-multiplicacion.onclick = () => {
-  display.innerHTML += "*";
-}
-
-// Insertar /
-division.onclick = () =>{
-  display.innerHTML += "/";
-}
-
-// Insertar **
-exponencial.onclick = () =>{
-  display.innerHTML += "**";
-}
-
-// Evaluar la expresion
+// Evaluamos la expresión para el igual
 igual.onclick = () => {
-  display.innerHTML = eval(display.innerHTML);
-}
-
+    display.innerHTML = eval(display.innerHTML);
+  }
+  
 // Poner a cero la expresion
 ac.onclick = () => {
   display.innerHTML = "0";
 }
-
-borrar.onclick = () =>{
-  var length = display.innerHTML.length;
-  //display.innerHTML = "0";
-  if (display.innerHTML.length > 1) {
-    display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length - 1);
-  }else if (display.innerHTML.length = 1) {
-    display.innerHTML = "0";
-  }
+  
+// Eliminar solo un dígito
+del.onclick = () => {
+  display.innerHTML = display.innerHTML.slice(0,-1);
 }
